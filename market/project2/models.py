9aeae624cd38178b729f6569eb48c7ab3b8e84c4
Glenn Field <gfield.va@gmail.com>
2017-09-20 18:27:07 -0400

    Add basic models

--- market/project2/models.py
+++ market/project2/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    birth_year = models.IntegerField()
+    tokens = models.IntegerField()
+
+
+class Lottery(models.Model):
+    desc = models.CharField(max_length=200)
+    users = models.ManyToManyField(User)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+
+class Bid(models.Model):
+    lottery = models.ForeignKey(Lottery, models.CASCADE)
+    user = models.ForeignKey(User, models.CASCADE)
+    value = models.IntegerField()
+
+
+class Card(models.Model):
+    lottery = models.ForeignKey(Lottery, models.SET_NULL)
+    game = models.CharField(max_length=200)
+    value = models.IntegerField(default=0)
