c22674c71302ac1b875c8afdd3a81bbf1591f890
Glenn Field <gfield.va@gmail.com>
2017-10-05 12:46:04 -0400

    Move site home page from entity API to web API

--- market/project2/urls.py
+++ market/project2/urls.py
@@ -2,9 +2,7 @@ from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 from . import views
 
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-]
+urlpatterns = []
 
 router = DefaultRouter()
 router.register(r'users', views.PersonViewSet)
