577a68dc1c80e2dd5c02313ad37d27eaa9ea80e3
Glenn Field <gfield.va@gmail.com>
2017-10-05 15:44:02 -0400

    Add basic home page Django template

--- web/web_app/views.py
+++ web/web_app/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index of our lottery site.")
+    # FIXME: this should query the experience API, but for now it is left blank
+    lotteries_list = []
+    template = loader.get_template('web_app/index.html')
+    context = {
+        'lotteries_list': lotteries_list,
+    }
+    return HttpResponse(template.render(context, request))
