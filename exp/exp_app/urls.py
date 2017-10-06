0e1d798e0e7cd1e1d40d47a9af170d9f0485a680
Danial Hussain <dhussain101@gmail.com>
2017-10-05 22:26:08 -0400

    connecting layers

--- exp/exp_app/urls.py
+++ exp/exp_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^lottery-pane$', views.lotteryPane, name='lottery-pane'),
+    url(r'^card-pane$', views.cardPane, name='card-pane'),
+    url(r'^lottery-detail/(.*)', views.lotteryDetail, name='lottery-detail'),
+    url(r'^card-detail/(.*)', views.cardDetail, name='card-detail'),
 ]
--- exp/exp_app/views.py
+++ exp/exp_app/views.py
@@ -5,6 +5,24 @@ import requests
 
 
 def lotteryPane(request):
-    r = requests.get('http://models-api:8000/lottery')
+    r = requests.get('http://models-api:8000/lotteries')
+    response = JsonResponse(r.json(), safe=False)
+    return response
+
+
+def cardPane(request):
+    r = requests.get('http://models-api:8000/cards')
+    response = JsonResponse(r.json(), safe=False)
+    return response
+
+
+def lotteryDetail(request, pk):
+    r = requests.get('http://models-api:8000/lottery/' + pk)
+    response = JsonResponse(r.json(), safe=False)
+    return response
+
+
+def cardDetail(request, pk):
+    r = requests.get('http://models-api:8000/card/' + pk)
     response = JsonResponse(r.json(), safe=False)
     return response
