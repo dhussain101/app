1a045b15bd1d7189b02a8fa21d963f71e94aeb50
Ellis Tsung <git@tsung.co>
2017-10-05 22:24:46 -0400

    Add scss

--- web/web_app/views.py
+++ web/web_app/views.py
@@ -1,14 +1,11 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 import requests
 
 
 def index(request):
     r = requests.get('http://exp-api:8000/lottery-pane')
-    print(r)
     lotteries_list = r.json()
-    template = loader.get_template('web_app/index.html')
     context = {
         'lotteries_list': lotteries_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
