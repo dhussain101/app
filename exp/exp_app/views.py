59cabc543f2f8ef1d2a0b5529e701fa4e7bff9db
Ellis Tsung <git@tsung.co>
2017-10-08 01:05:04 -0400

    Add detail fetching to exp layer

--- exp/exp_app/views.py
+++ exp/exp_app/views.py
@@ -1,26 +1,39 @@
 from django.http import JsonResponse
 import requests
+MODEL_URL = 'http://models-api:8000'
+
+
+def get(*paths):
+    """
+    Performs a get request on the models API layer.
+    For example, get('hello', 'world') will query http://models-api:8000/hello/world
+    :param paths: each sub-path entered as a separate argument
+    :return: JSON response from models API
+    """
+    return requests.get(MODEL_URL + ''.join('/{}' for _ in range(len(paths))).format(*paths)).json()
 
 
 def lottery_pane(request):
-    r = requests.get('http://models-api:8000/lotteries')
-    response = JsonResponse(r.json(), safe=False)
-    return response
+    response = get('lotteries')
+    return JsonResponse(response, safe=False)
 
 
 def card_pane(request):
-    r = requests.get('http://models-api:8000/cards')
-    response = JsonResponse(r.json(), safe=False)
-    return response
+    response = get('cards')
+    return JsonResponse(response, safe=False)
 
 
 def lottery_detail(request, pk):
-    r = requests.get('http://models-api:8000/lotteries/' + pk)
-    response = JsonResponse(r.json(), safe=False)
-    return response
+    response = get('lotteries', pk)
+    if response:
+        response['participants'] = list(map(
+            lambda user: get('users', user),
+            response['participants']))
+    return JsonResponse(response, safe=False)
 
 
 def card_detail(request, pk):
-    r = requests.get('http://models-api:8000/cards/' + pk)
-    response = JsonResponse(r.json(), safe=False)
-    return response
+    response = get('cards', pk)
+    if response:
+        response['game'] = get('games', response['game'])
+    return JsonResponse(response, safe=False)
