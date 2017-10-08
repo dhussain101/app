c814324cc6e3f06d8d17e4fa751ea1cd9d831b88
Danial Hussain <dhussain101@gmail.com>
2017-10-07 23:36:18 -0400

    start of exp and model layer unit tests

--- exp/exp_app/tests.py
+++ exp/exp_app/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class GetLotteriesTestCase(TestCase):
+    def setUp(self):
+        pass  # nothing to set up
+
+    def test_success_response(self):
+        response = self.client.get('http://models-api:8000/lotteries')
+        self.assertEqual(200, response.status_code)
+
+    def tearDown(self):
+        pass  # nothing to tear down
+
+
+class GetCardsTestCase(TestCase):
+    def setUp(self):
+        pass  # nothing to set up
+
+    def test_success_response(self):
+        response = self.client.get('http://models-api:8000/cards')
+        self.assertEqual(200, response.status_code)
+
+    def tearDown(self):
+        pass  # nothing to tear down
+
+
+class GetLotteryDetailsTestCase(TestCase):
+    def setUp(self):
+        pass  # nothing to set up
+
+    def test_success_response(self):
+        response = self.client.get('http://models-api:8000/lotteries/1')
+        self.assertEqual(200, response.status_code)
+
+    def tearDown(self):
+        pass  # nothing to tear down
+
+
+class GetCardDetailsTestCase(TestCase):
+    def setUp(self):
+        pass  # nothing to set up
+
+    def test_success_response(self):
+        response = self.client.get('http://models-api:8000/cards/1')
+        self.assertEqual(200, response.status_code)
+
+    def tearDown(self):
+        pass  # nothing to tear down
--- models/models_app/tests.py
+++ models/models_app/tests.py
@@ -3,8 +3,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from django.core.urlresolvers import reverse
 from datetime import datetime
-
-# Create your tests here.
+from .models import Person, Lottery, Bid, Game, Card
 
 
 class CreatePersonTestCase(TestCase):
@@ -25,5 +24,95 @@ class CreatePersonTestCase(TestCase):
             format='json')
 
     def test_api_can_create_a_person(self):
+        """Test the api has bucket creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+
+class CreateLotteryTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.lottery_data = {
+            'title': 'sample_lottery',
+            'description': 'this is a test lottery',
+            'participants': '',
+            'start_time': datetime.now(),
+            'end_time': datetime.now(),
+            'date_created': datetime.now()
+        }
+        self.response = self.client.post(
+            reverse('create'),
+            self.lottery_data,
+            format='json')
+
+    def test_api_can_create_a_lottery(self):
+        """Test the api has bucket creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+
+class CreateBidTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.bid_data = {
+            'first_name': 'John',
+            'last_name': 'Smith',
+            'username': 'test_jsmith2017',
+            'birthday': datetime.now()
+        }
+        self.response = self.client.post(
+            reverse('create'),
+            self.bid_data,
+            format='json')
+
+    def test_api_can_create_a_bid(self):
+        """Test the api has bucket creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+
+class CreateGameTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.game_data = {
+            'first_name': 'John',
+            'last_name': 'Smith',
+            'username': 'test_jsmith2017',
+            'birthday': datetime.now()
+        }
+        self.response = self.client.post(
+            reverse('create'),
+            self.game_data,
+            format='json')
+
+    def test_api_can_create_a_game(self):
+        """Test the api has bucket creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+
+class CreateCardTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.card_data = {
+            'first_name': 'John',
+            'last_name': 'Smith',
+            'username': 'test_jsmith2017',
+            'birthday': datetime.now()
+        }
+        self.response = self.client.post(
+            reverse('create'),
+            self.card_data,
+            format='json')
+
+    def test_api_can_create_a_card(self):
         """Test the api has bucket creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
