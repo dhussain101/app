708bf78753a79faf8384ee79860ea125dd603b28
Ellis Tsung <git@tsung.co>
2017-10-08 01:04:33 -0400

    Improve robustness of url matching

--- exp/exp_app/urls.py
+++ exp/exp_app/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^lottery-pane$', views.lottery_pane, name='lottery-pane'),
-    url(r'^card-pane$', views.card_pane, name='card-pane'),
-    url(r'^lottery-detail/(.*)', views.lottery_detail, name='lottery-detail'),
-    url(r'^card-detail/(.*)', views.card_detail, name='card-detail'),
+    url(r'^lottery-pane/?$', views.lottery_pane, name='lottery-pane'),
+    url(r'^card-pane/?$', views.card_pane, name='card-pane'),
+    url(r'^lottery-detail/([0-9]+)/?$', views.lottery_detail, name='lottery-detail'),
+    url(r'^card-detail/([0-9]+)/?$', views.card_detail, name='card-detail'),
 ]
