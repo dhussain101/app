2b47ce582b2b69dc3839bdca4fbc8d11c8f8efb2
Ellis Tsung <git@tsung.co>
2017-10-19 15:54:37 -0400

    Create styled login view with form

--- web/web_app/urls.py
+++ web/web_app/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . import views
+from . import views, views_auth
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -7,5 +7,9 @@ urlpatterns = [
     url(r'^lotteries/([0-9]+)/?$', views.lottery_detail, name='lottery-detail'),
     url(r'^cards/?$', views.cards, name='cards'),
     url(r'^cards/([0-9]+)/?$', views.card_detail, name='card-detail'),
+
+    url(r'^login/?$', views_auth.login, name='login'),
+    url(r'^register/?$', views_auth.register, name='register-account'),
+
     url(r'^.*$', views.bad_url, name='404'),
 ]
---
+++ web/web_app/views_auth.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .views import get
+from .forms import *
+import requests
+
+
+def login(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = LoginForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+
+            # TODO: the following...
+            # - check if username exists, if not error
+            # - check if password valid, if not error
+
+            # redirect to a new URL:
+            return HttpResponseRedirect('/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = LoginForm()
+
+    context = {
+        'title': 'Login',
+        'form': form,
+    }
+    return render(request, 'login.html', context)
+
+
+def register(request):
+    pass
