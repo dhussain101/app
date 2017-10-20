87d02fa809fc815a402fd0f8182e3d10dfdbb08e
Glenn <Glenn Field>
2017-10-20 00:12:27 -0400

    Fix models.py class ordering syntax bug

--- models/models_app/models.py
+++ models/models_app/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
 
-class Authenticator(models.Model):
-    user_id = models.ForeignKey(Person, models.PROTECT)
-    authenticator = models.BigIntegerField(primary_key=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-
-
 class Person(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
@@ -17,6 +11,13 @@ class Person(models.Model):
 
     def __str__(self):
         return ' '.join(['(username)', self.username, '(currency)', str(self.currency)])
+
+
+class Authenticator(models.Model):
+    user_id = models.ForeignKey(Person, models.PROTECT)
+    authenticator = models.BigIntegerField(primary_key=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
 
 
 class Lottery(models.Model):
