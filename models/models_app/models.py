77d25ad05f72c701cad5f6e98a7dabac818facfa
Glenn Field <gfield.va@gmail.com>
2017-10-19 23:45:12 -0400

    Add Authenticator model

--- models/models_app/models.py
+++ models/models_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class Authenticator(models.Model):
+    user_id = models.CharField(max_length=20)
+    authenticator = models.BigIntegerField(primary_key=True)
+    date_created = models.DateTimeField()
+
+
 class Person(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
