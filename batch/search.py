1620e68ad1860b3cc26f5776140d76fad56ba551
Glenn Field <gfield.va@gmail.com>
2017-10-31 14:52:11 -0400

    Load fixture data into the search database
    Also add a new script for killing all docker containers.

--- batch/search.py
+++ batch/search.py
@@ -1,8 +1,11 @@
 from kafka import KafkaConsumer
 from kafka.common import NodeNotReadyError
+from elasticsearch.exceptions import ConnectionError
 from elasticsearch import Elasticsearch
 import json
 import time
+import socket
+import sys
 
 
 def es_add(new_lottery, es_hook, es_index):
@@ -13,6 +16,14 @@ def es_add(new_lottery, es_hook, es_index):
     es_hook.indices.refresh(index=es_index)
 
 
+def load_fixtures(es_hook):
+    with open('../data/db.json', 'r') as db:
+        data = json.loads(db.read())
+    for data_row in data:
+        if data_row['model'] == 'models_app.lottery':
+            es_add(data_row['fields'], es_hook, 'lottery_index')
+
+
 def listen(es_hook, kafka_hook):
     for message in kafka_hook:
         # add each new addition to ES
@@ -21,13 +32,29 @@ def listen(es_hook, kafka_hook):
 
 
 def try_connect():
+    # use socket to attempt to connect to es
+    while True:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((str(sys.argv[1]), 9200))
+        if result == 0:
+            break
+        else:
+            time.sleep(3)
+
+    # we should be connected by now
+    es = Elasticsearch(['es'])
+
+    # using try/catch seems to work better for kafka, as it dies/restarts
+    # which can trick the socket check into thinking it's on when
+    # in reality it just turned back off
     while True:
         try:
             consumer = KafkaConsumer('lotteries', group_id='lottery-indexer', bootstrap_servers=['kafka:9092'])
-            es = Elasticsearch(['es'])
             break
         except NodeNotReadyError:
             time.sleep(3)
+
+    load_fixtures(es)
     listen(es, consumer)
 
 
