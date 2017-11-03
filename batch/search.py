380ef30a3969fba2086b407791a22ffea4960533
Glenn Field <gfield.va@gmail.com>
2017-11-03 04:38:53 -0400

    Allow user to add & search for new cards

--- batch/search.py
+++ batch/search.py
@@ -8,11 +8,11 @@ from kafka import KafkaConsumer
 from kafka.common import NodeNotReadyError
 
 
-def es_add(pk, new_lottery, es_hook, es_index, refresh=False):
+def es_add(pk, new_listing, es_hook, es_index, refresh=False):
     # FIX: for now use 'title' as the id, but I think this should use the serializer's id instead so that
     # titles don't have to be unique. This would require the id data to be returned by the post request
     # in the exp layer after the new lottery has been assigned a unique id in the model layer
-    es_hook.index(index=es_index, doc_type='listing', id=pk, body=new_lottery)
+    es_hook.index(index=es_index, doc_type='listing', id=pk, body=new_listing)
     if refresh:
         es_hook.indices.refresh(index=es_index)
 
@@ -31,15 +31,24 @@ def load_fixtures(es_hook):
 def listen(es_hook, kafka_hook):
     for message in kafka_hook:
         # add each new addition to ES
-        new_lottery = json.loads(message.value.decode('utf-8'))
-        pk = new_lottery.pop('id', new_lottery['title'])
-        new_lottery['start_time'] = new_lottery['start_time'].replace(' ', 'T', 1)
-        new_lottery['end_time'] = new_lottery['end_time'].replace(' ', 'T', 1)
-        print(new_lottery)
-        try:
-            es_add(pk, new_lottery, es_hook, 'lottery_index', refresh=True)
-        except:
-            print('Elastic Search could not add the following lottery:', new_lottery)
+        new_listing = json.loads(message.value.decode('utf-8'))
+        pk = new_listing.pop('id', new_listing['title'])
+
+        # NOTE: there is probably a better way to differentiate between cards and lotteries...
+        if 'start_time' in new_listing:
+            new_listing['start_time'] = new_listing['start_time'].replace(' ', 'T', 1)
+            new_listing['end_time'] = new_listing['end_time'].replace(' ', 'T', 1)
+            print(new_listing)
+            try:
+                es_add(pk, new_listing, es_hook, 'lottery_index', refresh=True)
+            except:
+                print('Elastic Search could not add the following lottery:', new_listing)
+        else:
+            print(new_listing)
+            try:
+                es_add(pk, new_listing, es_hook, 'card_index', refresh=True)
+            except:
+                print('Elastic Search could not add the following card:', new_listing)
 
 
 def try_connect():
@@ -63,7 +72,7 @@ def try_connect():
     # in reality it just turned back off
     while True:
         try:
-            consumer = KafkaConsumer('lotteries', group_id='lottery-indexer', bootstrap_servers=['kafka:9092'])
+            consumer = KafkaConsumer('new_listing', group_id='indexer', bootstrap_servers=['kafka:9092'])
             break
         except NodeNotReadyError:
             time.sleep(3)
